5a5c1271a657969005e1c39baf549dbbb6813aa7
fantasira <sirareddit@gmail.com>
2025-03-06 23:49:52 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+        <div class="container">
+            <h1>Portfolio de Fanta Diallo</h1>
+            <nav>
+                <a href="eportefolio.php">Accueil</a>
+                <a href="realisations.php" target="_blank">Projets</a>
+                <a href="certification.php">Certification</a>
+            </nav>
+        </div>
+    </header>
